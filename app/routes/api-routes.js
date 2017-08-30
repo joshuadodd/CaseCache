fb408db173a0acc143abc0b580716c50e9f00826
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-08-30 16:45:26 -0400

    html update

---
+++ app/routes/api-routes.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = function (app, passport) {
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/signup'
+    }
+    ));
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/signin'
+    }
+    ));
+
+
+
+
+}
+
+
+
+
+
+
---
+++ app/routes/html-routes.js
@@ -0,0 +1,34 @@
+var path = require("path");
+module.exports = function (app, passport) {
+
+    app.get('/', function (req, res) {
+        res.send('Welcome to Passport with Sequelize');
+    });
+
+    app.get('/signup', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+
+
+    app.get('/signin', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/signin.html"));
+    });
+
+    app.get('/dashboard', isLoggedIn, function (req, res) {
+        res.send('Yay!');
+    });
+
+    app.get('/logout', function (req, res) {
+
+        req.session.destroy(function (err) {
+            res.redirect('/');
+        });
+    });
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+
+        res.redirect('/signin');
+    }
+}
