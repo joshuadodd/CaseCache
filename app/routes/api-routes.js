a0c1a075cde542f50afc4a1c56668286e06ab383
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-09-02 07:35:26 -0400

    trying out a new timeline

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,25 +1,16 @@
 var path = require("path");
 
-module.exports = function (app, passport) {
+module.exports = function(app, passport) {
 
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/dashboard',
         failureRedirect: '/signup'
-    }
-    ));
+    }));
     app.post('/signin', passport.authenticate('local-signin', {
         successRedirect: '/dashboard',
         failureRedirect: '/signin'
-    }
-    ));
-
+    }));
 
 
 
 }
-
-
-
-
-
-
